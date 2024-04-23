898269bf721b2d593143c6afb10e4ae663d31431
Javier Aceña <40375118+j0nl1@users.noreply.github.com>
2024-04-23 09:27:10 +0200

    feat: feature support in workspace (#3)
    * feat: feature support in workspace under `metadata = {  build_variants = [VARIANT] }`
    
    * fix: building script

--- bob_the_builder/src/lib.rs
+++ bob_the_builder/src/lib.rs
@@ -7,7 +7,7 @@ use std::{
     process::Command,
 };
 
-use cargo_toml::{is_workspace, IsWorkspace};
+use cargo_toml::{is_workspace, CargoToml, IsWorkspace};
 
 const CARGO_PATH: &str = "cargo";
 const PACKAGE_PREFIX: &str = "contracts/";
@@ -61,8 +61,51 @@ pub fn build_workspace(workspace_members: &[String]) {
 
     println!("Contracts to be built: {:?}", contract_packages);
 
+    let build_dir = PathBuf::from("/target/wasm32-unknown-unknown/release");
+
     for contract in contract_packages {
         println!("Building {:?} ...", contract);
+        let cargo_file = fs::read_to_string(contract.join("Cargo.toml")).unwrap();
+
+        let CargoToml { package, .. } = toml::from_str(&cargo_file).unwrap();
+
+        let package = package.expect("package is required in Cargo.toml");
+        let name = package.name.replace("-", "_");
+        let variants = package
+            .metadata
+            .unwrap_or_default()
+            .build_variants
+            .unwrap_or_default();
+
+        if variants.len() > 0 {
+            for variant in variants {
+                let mut child = Command::new(CARGO_PATH)
+                    .args(&[
+                        "build",
+                        "--target-dir=/target",
+                        "--release",
+                        "--features",
+                        variant.as_str(),
+                        "--lib",
+                        "--target=wasm32-unknown-unknown",
+                        "--locked",
+                    ])
+                    .env("RUSTFLAGS", "-C link-arg=-s")
+                    .current_dir(canonicalize(contract).unwrap())
+                    .spawn()
+                    .unwrap();
+                let error_code = child.wait().unwrap();
+                assert!(error_code.success());
+
+                println!("Built variant: {}", variant);
+
+                fs::rename(
+                    build_dir.join(format!("{name}.wasm")),
+                    build_dir.join(format!("{name}_{variant}.wasm")),
+                )
+                .unwrap();
+            }
+        }
 
         let mut child = Command::new(CARGO_PATH)
             .args(&[
